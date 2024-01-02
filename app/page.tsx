59b50e89a43b56dbb7ff41fdbcc902dec19abc02
Prashant Sihag <prashantsihag0309@gmail.com>
2024-01-02 19:18:04 +1030

    finished course

--- app/page.tsx
+++ app/page.tsx
@@ -23,21 +23,16 @@ export default function Page() {
           <p
             className={`text-xl text-gray-800 md:text-3xl md:leading-normal ${lusitana.className} antialiased`}
           >
-            <strong>Welcome.</strong>
-            <br />
-            <span className={`text-blue-500`}>
-              Please enter username below and click generate.
-            </span>
-            {/* <a href="https://nextjs.org/learn/" className="text-blue-500">
+            <strong>Welcome. </strong>
+            <a href="https://nextjs.org/learn/" className="text-blue-500">
               Next.js Learn Course
-            </a> */}
+            </a>
           </p>
-          <input type="text" required placeholder="Github Username" />
           <Link
             href="/login"
             className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
           >
-            <span>Generate</span> <ArrowRightIcon className="w-5 md:w-6" />
+            <span>Login</span> <ArrowRightIcon className="w-5 md:w-6" />
           </Link>
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
